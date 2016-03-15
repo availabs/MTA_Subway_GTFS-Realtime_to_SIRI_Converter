0b121ae388f61c4a3350b7c175723dcb743d2431
paultomchik <paultomchik@gmail.com>
2016-03-15 15:32:03 -0400

    GTFS and GTFS-Realtime configuration diagnostic events, ability to start and stop Converter from server admin console.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict' ;
+
 /**
  * MTA_Subway_GTFS-Realtime_to_SIRI_Converter converts MTA GTFS-Realtime feed messages into SIRI format.
  *
@@ -7,8 +9,12 @@
  */
 
 module.exports = {
-    ConverterStream                  : require('./lib/ConverterStream.js')         ,
+    ConverterStream                   : require('./lib/ConverterStream.js') ,
 
-    MTA_Subway_GTFS_Toolkit          : require('MTA_Subway_GTFS_Toolkit')          ,
-    MTA_Subway_GTFS_Realtime_Toolkit : require('MTA_Subway_GTFS-Realtime_Toolkit') ,
+    MTA_Subway_GTFS_Toolkit           : require('MTA_Subway_GTFS_Toolkit') ,
+    MTA_Subway_GTFS_Realtime_Toolkit  : require('MTA_Subway_GTFS-Realtime_Toolkit') ,
+
+    ConverterEventEmitter             : require('./lib/events/ConverterEventEmitter') ,
+    GTFS_ToolkitEventEmitter          : require('MTA_Subway_GTFS_Toolkit').ToolkitEventEmitter ,
+    GTFS_Realtime_ToolkitEventEmitter : require('MTA_Subway_GTFS-Realtime_Toolkit').ToolkitEventEmitter ,
 };
